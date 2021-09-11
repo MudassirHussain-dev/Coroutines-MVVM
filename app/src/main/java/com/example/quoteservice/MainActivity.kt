2d5521134d0bb92d9d45912319f7fa19f7051290
AAM POWER DEVELOPER <AAM POWER DEVELOPER@DESKTOP-FLAPIQK>
2021-09-11 11:53:50 +0500

    MVVM COROUTINES

---
+++ app/src/main/java/com/example/quoteservice/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.quoteservice
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.cheezycode.randomquote.QuoteApplication
+import com.cheezycode.randomquote.viewmodels.MainViewModel
+import com.cheezycode.randomquote.viewmodels.MainViewModelFactory
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var mainViewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val repository = (application as QuoteApplication).quoteRepository
+
+        mainViewModel = ViewModelProvider(this, MainViewModelFactory(repository)).get(MainViewModel::class.java)
+
+        mainViewModel.quotes.observe(this, Observer {
+            Toast.makeText(this@MainActivity, it.results.size.toString(), Toast.LENGTH_SHORT).show()
+        })
+    }
+}
---
+++ app/src/main/java/com/example/quoteservice/utils/NetworkUtils.kt
@@ -0,0 +1,26 @@
+package com.cheezycode.randomquote.utils
+
+import android.content.Context
+import android.content.Context.CONNECTIVITY_SERVICE
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+
+class NetworkUtils {
+
+    companion object{
+
+        fun isInternetAvailable(context: Context): Boolean {
+            (context.getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager).run {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    return this.getNetworkCapabilities(this.activeNetwork)?.hasCapability(
+                        NetworkCapabilities.NET_CAPABILITY_INTERNET
+                    ) ?: false
+                } else {
+                    (@Suppress("DEPRECATION")
+                    return this.activeNetworkInfo?.isConnected ?: false)
+                }
+            }
+        }
+    }
+}
